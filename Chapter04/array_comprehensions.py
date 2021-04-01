b04e786a00933822e5824f2c6f4a3eeeb4e8b960
jin <hyojinsim90@gmail.com>
2021-04-01 23:51:44 +0900

    Modified Chapter04

--- Chapter04/array_comprehensions.py
+++ Chapter04/array_comprehensions.py
@@ -2,4 +2,4 @@ array = ["사과", "자두", "초콜릿", "바나나", "체리"]
 output = [fruit for fruit in array if fruit != "초콜릿"]
 # array의 요소를 fruit이라고 할 때 초콜릿이 아닌 fruit으로 리스트를 재조합해 주세요
 
-print(output) # ['사과', '자두', '바나나', '체리']
+print(output) # ['사과', '자두', '바나나', '체리']sss
